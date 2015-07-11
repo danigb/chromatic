e18f376f090aa1427048b8fdee66eacd55bf3354
danigb <danigb@gmail.com>
2015-07-12 01:32:00 +0200

    SCALES const removed from function

--- chromatic.js
+++ chromatic.js
@@ -1,24 +1,24 @@
 'use strict'
 
+var SCALES = [
+  'C Db D Eb E F Gb G Ab A Bb B'.split(' '),
+  'Cb Dbb Db Ebb Eb Fb Gbb Gb Abb Ab Bbb Bb'.split(' '),
+  'C C# D D# E F F# G G# A A# B'.split(' '),
+  'C# C## D# D## E# F# F## G# G## A# A## B#'.split(' ')
+]
+
 function Chromatic (root, octave, length, descending) {
   if (!root) throw Error('Give me a root, please')
   length = length || 12
   root = root.charAt(0).toUpperCase() + root.slice(1).toLowerCase()
   descending = descending === true ? 1 : 0
 
-  return Chromatic.SCALES.reduce(function (all, notes) {
+  return SCALES.reduce(function (all, notes) {
     var scale = find(root, notes)
     if (scale) all.push(octavize(forceLength(reverse(scale, descending), length), octave, descending))
     return all
   }, [])
 }
-
-Chromatic.SCALES = [
-  'C Db D Eb E F Gb G Ab A Bb B'.split(' '),
-  'Cb Dbb Db Ebb Eb Fb Gbb Gb Abb Ab Bbb Bb'.split(' '),
-  'C C# D D# E F F# G G# A A# B'.split(' '),
-  'C# C## D# D## E# F# F## G# G## A# A## B#'.split(' ')
-]
 
 function find (root, notes) {
   var index = notes.indexOf(root)
